5867c6c7c8d6560d42cc76570d7f194bda7c1040
john makateto <jmakateto@gmail.com>
2023-08-29 11:04:18 +0300

    Completed spicy foods project

--- lib/data_structures.py
+++ lib/data_structures.py
@@ -1,38 +1,46 @@
-spicy_foods = [
-    {
-        "name": "Green Curry",
-        "cuisine": "Thai",
-        "heat_level": 9,
-    },
-    {
-        "name": "Buffalo Wings",
-        "cuisine": "American",
-        "heat_level": 3,
-    },
-    {
-        "name": "Mapo Tofu",
-        "cuisine": "Sichuan",
-        "heat_level": 6,
-    },
-]
-
 def get_names(spicy_foods):
-    pass
+    names = []
+    for food in spicy_foods:
+        names.append(food["name"])
+    return names
 
 def get_spiciest_foods(spicy_foods):
-    pass
+    spiciest = []
+    for food in spicy_foods:
+        if food["heat_level"] > 5:
+            spiciest.append(food)
+    return spiciest
 
 def print_spicy_foods(spicy_foods):
-    pass
+    for food in spicy_foods:
+        heat_level = "🌶" * food["heat_level"]
+        print(f"{food['name']} ({food['cuisine']}) | Heat Level: {heat_level}")
 
 def get_spicy_food_by_cuisine(spicy_foods, cuisine):
-    pass
+    for food in spicy_foods:
+        if food["cuisine"] == cuisine:
+            return food
 
 def print_spiciest_foods(spicy_foods):
-    pass
+    spiciest = get_spiciest_foods(spicy_foods)
+    print_spicy_foods(spiciest)
 
-def get_average_heat_level(spicy_foods):
-    pass
+def average_heat_level(spicy_foods):
+    total_heat = sum([food["heat_level"] for food in spicy_foods])
+    return total_heat // len(spicy_foods)
 
-def create_spicy_food(spicy_foods, spicy_food):
-    pass
+# Sample data
+spicy_foods = [
+    {"name": "Green Curry", "cuisine": "Thai", "heat_level": 9},
+    {"name": "Buffalo Wings", "cuisine": "American", "heat_level": 3},
+    {"name": "Mapo Tofu", "cuisine": "Sichuan", "heat_level": 6}
+]
+
+# Test your functions
+if __name__ == "__main__":
+    print(get_names(spicy_foods))
+    print(get_spiciest_foods(spicy_foods))
+    print_spicy_foods(spicy_foods)
+    print(get_spicy_food_by_cuisine(spicy_foods, "American"))
+    print_spiciest_foods(spicy_foods)
+    print(average_heat_level(spicy_foods))
